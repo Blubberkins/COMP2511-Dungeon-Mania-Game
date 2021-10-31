b605a19aca1bf1ae6be0f7f680101cd80df6d379
greninjaaa <liamheng1@gmail.com>
2021-10-31 16:37:42 +1100

    made one ring logically make sense with singleton pattern

--- src/main/java/dungeonmania/TheOneRingEntity.java
+++ src/main/java/dungeonmania/TheOneRingEntity.java
@@ -3,13 +3,18 @@ package dungeonmania;
 import dungeonmania.util.Position;
 
 public class TheOneRingEntity extends CollectableEntities {
+    private static TheOneRingEntity single_instance = null;
 
-    public TheOneRingEntity(Position pos, String type, String id) {
+    private TheOneRingEntity(Position pos, String type, String id) {
         super(pos, type, id);
-        //TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
     }
 
+    public static synchronized TheOneRingEntity getInstance(Position pos, String type, String id) {
+        if (single_instance == null) {
 
-    
+            single_instance = new TheOneRingEntity(pos, type, id);
+        }
+        return single_instance;
+    }
 }
-
