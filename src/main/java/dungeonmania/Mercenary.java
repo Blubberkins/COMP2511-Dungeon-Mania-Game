4971ca351669b0835b78bc37b691eaf225b7ddcb
greninjaaa <liamheng1@gmail.com>
2021-11-08 16:27:00 +1100

    set framework for new mercenary movement; deleted old vector based logic (can be restored if needed)

--- src/main/java/dungeonmania/Mercenary.java
+++ src/main/java/dungeonmania/Mercenary.java
@@ -1,10 +1,13 @@
 package dungeonmania;
 
+import dungeonmania.util.Dijkstra;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
 
 // By Liam
@@ -44,115 +47,15 @@ public class Mercenary extends MovingEntity {
         if (player == null) {
             return;
         }
-        // get the position vector
-        Position vector = Position.calculatePositionBetween(this.getPos(), player.getPos());
 
-        // based on the vector, decide which direction is best to move in
-        List<Direction> order = optimalMove(vector, dungeonmania);
+        // based on the map, decide which direction is best to move in
+        // using dijkstra's algorithm
+        Map<Position, Position> optimal = Dijkstra.shortestPath(mapPositions(dungeonmania), this.getPos(),
+                dungeonmania);
 
-        // pick the single best move that is valid
-        if (!dungeonmania.getCharacter().getisInvincible()) {
-            for (Direction move : order) {
-                if (validMove(this.getPos(), move, dungeonmania)) {
-                    this.setPos(this.getPos().translateBy(move));
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < order.size(); i++) {
-                if (validMove(this.getPos(), order.get(order.size() - 1 - i), dungeonmania)) {
-                    this.setPos(this.getPos().translateBy(order.get(order.size() - 1 - i)));
-                    break;
-                }
-            }
-        }
+        // TODO trace the optimal path, then make the move
+        Stack<Position> path = tracePath(optimal, this.getPos(), getPlayer(dungeonmania).getPos());
 
-    }
-
-    /**
-     * Checks if a desired move is valid (not blocked)
-     * 
-     * @param position
-     * @param direction
-     * @param dungeonMania
-     * @return boolean
-     */
-    public Boolean validMove(Position position, Direction direction, DungeonMania dungeonMania) {
-        Position newPos = position.translateBy(direction);
-
-        List<Entity> entities = dungeonMania.getEntities();
-
-        for (Entity entity : entities) {
-            if (entity instanceof Wall && entity.getPos().equals(newPos)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Calculates the optimal movement
-     * 
-     * @param vector
-     * @param dungeonMania
-     * @return List<Direction>
-     */
-    public List<Direction> optimalMove(Position vector, DungeonMania dungeonMania) {
-        Boolean xPos = vector.getX() >= 0;
-        int xDist = Math.abs(vector.getX());
-        Boolean yPos = vector.getY() >= 0;
-        int yDist = Math.abs(vector.getY());
-
-        List<Direction> optimal = new ArrayList<Direction>();
-
-        if (xDist >= yDist) {
-            if (xPos) {
-                optimal.add(Direction.RIGHT);
-                if (yPos) {
-                    optimal.add(Direction.DOWN);
-                    optimal.add(Direction.UP);
-                } else {
-                    optimal.add(Direction.UP);
-                    optimal.add(Direction.DOWN);
-                }
-                optimal.add(Direction.LEFT);
-            } else {
-                optimal.add(Direction.LEFT);
-                if (yPos) {
-                    optimal.add(Direction.DOWN);
-                    optimal.add(Direction.UP);
-                } else {
-                    optimal.add(Direction.UP);
-                    optimal.add(Direction.DOWN);
-                }
-                optimal.add(Direction.RIGHT);
-            }
-        } else {
-            if (yPos) {
-                optimal.add(Direction.DOWN);
-                if (xPos) {
-                    optimal.add(Direction.RIGHT);
-                    optimal.add(Direction.LEFT);
-                } else {
-                    optimal.add(Direction.LEFT);
-                    optimal.add(Direction.RIGHT);
-                }
-                optimal.add(Direction.UP);
-            } else {
-                optimal.add(Direction.UP);
-                if (xPos) {
-                    optimal.add(Direction.RIGHT);
-                    optimal.add(Direction.LEFT);
-                } else {
-                    optimal.add(Direction.LEFT);
-                    optimal.add(Direction.RIGHT);
-                }
-                optimal.add(Direction.DOWN);
-            }
-        }
-
-        return optimal;
     }
 
     /**
@@ -206,10 +109,18 @@ public class Mercenary extends MovingEntity {
         return this.minBribe;
     }
 
-    public List<Position> mapPositions() {
+    public List<Position> mapPositions(DungeonMania game) {
         List<Position> grid = new ArrayList<Position>();
         // TODO: return all positions in the grid
         return grid;
+    }
+
+    public Stack<Position> tracePath(Map<Position, Position> optimal, Position src, Position dest) {
+        Stack<Position> path = new Stack<Position>();
+
+        path.add(dest);
+
+        return path;
     }
 
 }
